cecd172a1016c5bedb5e9a82ae041de75eb53cb0
raduandreiholban1-stack <raduandreiholban1@gmail.com>
2026-04-06 22:43:15 +0300

    Add files via upload

---
+++ assets/icons/rd_icons.dart/libiconsrd_icons.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class RDIcons {
+  // Rooms
+  static const living = CupertinoIcons.tv;
+  static const bedroom = CupertinoIcons.bed_double;
+  static const kitchen = CupertinoIcons.house;
+  static const bathroom = CupertinoIcons.drop;
+  static const hallway = CupertinoIcons.square_grid_2x2;
+
+  // Devices
+  static const light = CupertinoIcons.lightbulb;
+  static const power = CupertinoIcons.power;
+  static const temperature = CupertinoIcons.thermometer;
+  static const tv = CupertinoIcons.tv;
+  static const speaker = CupertinoIcons.speaker_2;
+  static const camera = CupertinoIcons.camera;
+  static const wifi = CupertinoIcons.wifi;
+  static const motion = CupertinoIcons.bolt;
+
+  // Controls
+  static const settings = CupertinoIcons.settings;
+  static const scenarios = CupertinoIcons.slider_horizontal_3;
+  static const sun = CupertinoIcons.sun_max;
+  static const moon = CupertinoIcons.moon;
+
+  // Security
+  static const lock = CupertinoIcons.lock;
+  static const unlock = CupertinoIcons.lock_open;
+}
